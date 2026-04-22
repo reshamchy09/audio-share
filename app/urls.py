956b1f2d214e3e7dc82313c8d994e8ddcd725d52
reshamchy09 <151758376+reshamchy09@users.noreply.github.com>
2026-04-22 18:42:20 +0545

    Update urls.py

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",          views.index,   name="index"),
+       path("", login_view, name="login"),
+    path("index",          views.index,   name="index"),
     path("control/",  views.control, name="control"),
 ]
